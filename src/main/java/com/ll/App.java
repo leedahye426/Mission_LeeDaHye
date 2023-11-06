06809d817c7d322f096342198cc5b61395a55be1
leedahye426 <9535957@naver.com>
2023-11-06 10:21:02 +0900

    2단계 등록 기능

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -3,16 +3,32 @@ package com.ll;
 import java.util.Scanner;
 
 public class App {
+    private final Scanner scanner;
     public App() {
-
+        scanner = new Scanner(System.in);
     }
     public void run() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
-        System.out.print("명령) ");
-        String cmd = scanner.nextLine();
-        if(cmd.equals("종료")) {
-            return;
+
+        while(true) {
+            System.out.print("명령) ");
+            String cmd = scanner.nextLine();
+
+            if(cmd.equals("종료")) {
+                return;
+            }
+            else if(cmd.equals("등록")) {
+                actionWrite();
+            }
         }
+
+    }
+
+    private void actionWrite() {
+        System.out.print("명언 : ");
+        String content = scanner.nextLine();
+
+        System.out.print("작가 : ");
+        String author = scanner.nextLine();
     }
 }
